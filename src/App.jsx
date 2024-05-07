d034ef7eaece37d47d485caede396344c6f7cdd9
MohitChaudhari <mohitchoudharu2@gmail.com>
2024-05-07 14:10:37 +0530

    functionality added

--- src/App.jsx
+++ src/App.jsx
@@ -4,54 +4,72 @@ import Sidebar from "./components/Sidebar";
 import "./App.css";
 
 function App() {
-  const [taskList , setTaskList] = useState([]);
+  const [taskList, setTaskList] = useState([]);
 
   const [labelValue, setLabelValue] = useState("");
-  
+
   const [taskValue, setTaskValue] = useState("");
-  
+
   const [showModal, setModal] = useState(false);
+
+  const[filteredTaskList,setFilteredTaskList] = useState([])
 
 
   const createTask = () => {
-    if(taskValue.trim() !== "") {
-    setTaskList([...taskList, {
-     label: labelValue,
-     task: taskValue, 
-     isCompleted: false,
-     time: new Date().toLocaleTimeString(), 
-     date: new Date().toLocaleDateString() }])
+    if (taskValue.trim() !== "") {
+      setTaskList([
+        ...taskList,
+        {
+          label: labelValue,
+          task: taskValue,
+          isCompleted: false,
+          time: new Date().toLocaleTimeString(),
+          date: new Date().toLocaleDateString(),
+        },
+      ]);
     }
     toggleModal();
     setLabelValue("");
     setTaskValue("");
-  }
+  };
 
   const labelInputValue = (e) => {
     setLabelValue(e.target.value);
     console.log(e.target.value);
-  }
+  };
   const taskInputValue = (e) => {
     setTaskValue(e.target.value);
     console.log(e.target.value);
-  }
+  };
 
-  useEffect(()=>{
-    setTaskList( localStorage.getItem("taskList") ? JSON.parse(localStorage.getItem("taskList")):[])
-  }, [])
+  useEffect(() => {
+    setTaskList(
+      localStorage.getItem("taskList")
+        ? JSON.parse(localStorage.getItem("taskList"))
+        : []
+    );
+  }, []);
 
-  useEffect(()=>{
-    localStorage.setItem("taskList", JSON.stringify(taskList))
-  },[taskList])
- 
+  useEffect(() => {
+    localStorage.setItem("taskList", JSON.stringify(taskList));
+  }, [taskList]);
 
   const handleDelete = (index) => {
-    let filteredArr = taskList.filter((item ,idx) => {
-      return  idx !== index
-    })
-    setTaskList(filteredArr)
-  }
-  
+    let filteredArr = taskList.filter((item, idx) => {
+      return idx !== index;
+    });
+    setTaskList(filteredArr);
+  };
+
+  const handleComplete = (index) => {
+    let filteredArr = taskList.map((item, id) => {
+      if (index === id) {
+        item.isCompleted = !item.isCompleted;
+      }
+      return item;
+    });
+    setTaskList(filteredArr);
+  };
 
   const toggleModal = () => {
     setModal(!showModal);
@@ -59,27 +77,49 @@ function App() {
 
   return (
     <div className="flex">
-      <Sidebar Toggle={createTask} /> {/* Pass addTask function instead of toggleModal */}
+      <Sidebar toggleModal={toggleModal} setTaskList={setTaskList}
+      taskList={taskList}
+      />{" "}
       <div className="p-10">
-        <Modal showModal={showModal} toggleModal={toggleModal} createTask = {createTask} taskInputValue={taskInputValue} labelInputValue={labelInputValue} labelValue={labelValue} taskValue={taskValue}/>
-        <button
+        <Modal
+          showModal={showModal}
+          toggleModal={toggleModal}
+          createTask={createTask}
+          taskInputValue={taskInputValue}
+          labelInputValue={labelInputValue}
+          labelValue={labelValue}
+          taskValue={taskValue}
+          setFilteredTaskList={setFilteredTaskList}
+        />
+        {/* <button
           className="addnew-button bg-blue-500 text-white rounded-md "
           onClick={toggleModal}
         >
           New task
-        </button>
+        </button> */}
 
-      <div id="DisplayContainer " className="mt-[20px]">
-        <h1 className="text-xl font-bold ">Today </h1>
-        <h1 className="mt-4 text-lg font-semibold mb-4">My Projects</h1>
-        {taskList.map((task,index) => (
-          <div key={index} className={`${task.isCompleted ? "line-through" : ""} flex  py-6 border-b-2 border-t-2 text-lg`}>{index+1}. Label: {task.label} || task: {task.task} || date: {task.date} || time:{task.time} 
-          <button onClick={() => handleDelete(index)}>❌</button></div>
-        ))}
-          
+        <div id="DisplayContainer " className="mt-[20px]">
+          <h1 className="text-xl font-bold ">Today </h1>
+          <h1 className="mt-4 text-lg font-semibold mb-4">My Projects</h1>
+          {taskList.map((task, index) => (
+            <div
+              key={index}
+              className={`
+              flex  py-6 border-b-2 border-t-2 text-lg`}
+            >
+              {index + 1}. Label: {task.label} || task: {task.task} || date:{" "}
+              {task.date} || time:{task.time}
+              <button onClick={() => handleDelete(index)}>❌</button>
+              <button
+                onClick={() => handleComplete(index)}
+                className="px-3 mx-1 bg-black text-white"
+              >
+                Done
+              </button>
+            </div>
+          ))}
+        </div>
       </div>
-      </div>
-
     </div>
   );
 }
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,26 +1,54 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { CiUser, CiHashtag } from "react-icons/ci";
+import { GrCompliance } from "react-icons/gr";
+import { IoSearch } from "react-icons/io5";
 import {
-  CiCirclePlus,
-  CiSearch,
-  CiCalendar,
-  CiUser,
-  CiHashtag,
-} from "react-icons/ci";
-import { VscInbox } from "react-icons/vsc";
+  MdOutlinePendingActions,
+  MdOutlineAddCircleOutline,
+  MdUpcoming,
+} from "react-icons/md";
 
-const Sidebar = ({Toggle}) => {
+const Sidebar = ({ toggleModal, setTaskList, taskList,setFilteredTaskList }) => {
   const [open, setOpen] = useState(true);
-  
+  const [menuVisible, setMenuVisible] = useState(false);
+
   const Menus = [
-    { title: "Add Task", icons: <CiCirclePlus />, color: "#DC4A3E", onClick : Toggle },
-    { title: "Search", icons: <CiSearch /> },
-    { title: "Inbox", icons: <VscInbox /> },
-    { title: "Upcoming", icons: <CiCalendar /> },
+    { title: "Add Task", icons: <MdOutlineAddCircleOutline />, color: "#DC4A3E" },
+    { title: "Search", icons: <IoSearch /> },
+    { title: "Completed", icons: <GrCompliance /> },
+    { title: "Pending", icons: <MdOutlinePendingActions /> },
+    { title: "All Task", icons: <MdUpcoming /> },
     { title: "My Projects", icons: "", gap: true, color: "red" },
     { title: "Fitness", icons: <CiHashtag />, logoColor: "yellow" },
     { title: "Groceries", icons: <CiHashtag />, logoColor: "red" },
     { title: "Appointments", icons: <CiHashtag />, logoColor: "blue" },
   ];
+
+  const handleAddTaskClick = () => {
+    toggleModal();
+  };
+
+  const handleCompleteClick = () => {
+    const completedTasks = taskList.filter(item => item.isCompleted);
+    setFilteredTaskList(completedTasks)
+  };
+
+  const handlePendingClick = () => {
+    const pendingTasks = taskList.filter(item => !item.isCompleted);
+    setTaskList(pendingTasks);
+  };
+
+  const handleAllTaskClick = () => {
+    setTaskList(taskList);
+  };
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setMenuVisible(true);
+    }, 400);
+
+    return () => clearTimeout(timeout);
+  }, [open]);
 
   return (
     <div className="flex">
@@ -51,36 +79,45 @@ const Sidebar = ({Toggle}) => {
           </h1>
         </div>
         <ul className="pt-6">
-          {Menus.map((Menu, index) => (
-            <li
-              key={index}
-              className={`flex  rounded-md p-2 cursor-pointer hover:bg-red-100 text-black text-sm items-center gap-x-2 font-semibold hover:text-red-500
-              ${Menu.gap ? "mt-9" : ""} ${index === 0 && "bg-light-white"}`}
-            >
-              <span
-                className={`text-xl font-extrabold ${
-                  Menu.color ? "text-red-500" : ""
-                }`}
-                style={{ color: Menu.logoColor || Menu.color }}
+          {menuVisible &&
+            Menus.map((Menu, index) => (
+              <li
+                key={index}
+                className={`flex  rounded-md p-2 cursor-pointer hover:bg-red-100 text-black text-sm items-center gap-x-2 font-semibold hover:text-red-500
+                ${Menu.gap ? "mt-9" : ""} ${index === 0 && "bg-light-white"}`}
+                onClick={() => {
+                  if (Menu.title === "Add Task") {
+                    handleAddTaskClick();
+                  } else if (Menu.title === "Completed") {
+                    handleCompleteClick();
+                  } else if (Menu.title === "Pending") {
+                    handlePendingClick();
+                  } else if (Menu.title === "All Task") {
+                    handleAllTaskClick();
+                  }
+                }}
               >
-                {Menu.icons}
-              </span>
-              <span
-                className={`${
-                  !open && "hidden"
-                } origin-left duration-200 text-xl font-semibold ${
-                  Menu.color ? "text-red-500" : ""
-                }`}
-              >
-                {Menu.title}
-              </span>
-            </li>
-          ))}
+                <span
+                  className={`text-xl font-extrabold ${
+                    Menu.color ? "text-red-500" : ""
+                  }`}
+                  style={{ color: Menu.logoColor || Menu.color }}
+                >
+                  {Menu.icons}
+                </span>
+                <span
+                  className={`${
+                    !open && "hidden"
+                  } origin-left duration-200 text-xl font-semibold ${
+                    Menu.color ? "text-red-500" : ""
+                  }`}
+                >
+                  {Menu.title}
+                </span>
+              </li>
+            ))}
         </ul>
       </div>
-      {/* <div className="h-screen flex-1 p-7">
-        <h1 className="text-2xl font-semibold ">To-do's go here</h1>
-      </div> */}
     </div>
   );
 };
